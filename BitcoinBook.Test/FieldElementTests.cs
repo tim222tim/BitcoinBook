32daaa3d78cf2ee483cdd86eeb91d1449fa402fb
Tim <tim@timsisland.com>
2020-01-19 09:19:15 -0500

    Add project files.

---
+++ BitcoinBook.Test/FieldElementTests.cs
@@ -0,0 +1,86 @@
+using System;
+using Xunit;
+
+namespace BitcoinBook.Test
+{
+    public class FieldElementTests
+    {
+        [Fact]
+        public void ContructorTest()
+        {
+            var element = new FieldElement(1, 3);
+            Assert.Equal(1, element.Number);
+            Assert.Equal(3, element.Prime);
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(1, 1)]
+        [InlineData(4, 3)]
+        [InlineData(-1, 3)]
+        public void OutOfRangeTest(long number, long prime)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new FieldElement(number, prime));
+        }
+
+        [Fact]
+        public void AddInvalidTest()
+        {
+            Assert.Throws<InvalidOperationException>(() => new FieldElement(1, 3).Add(new FieldElement(1, 5)));
+        }
+
+        [Theory]
+        [InlineData(3, 0, 0, 0)]
+        [InlineData(3, 1, 0, 1)]
+        [InlineData(3, 2, 2, 1)]
+        [InlineData(19, 11, 17, 9)]
+        public void AddTest(long prime, long number1, long number2, long result)
+        {
+            var element = new FieldElement(number1, prime).Add(new FieldElement(number2, prime));
+            Assert.Equal(result, element.Number);
+            Assert.Equal(prime, element.Prime);
+        }
+
+        [Theory]
+        [InlineData(57, 44, 33, 20)]
+        [InlineData(3, 1, 0, 1)]
+        [InlineData(3, 2, 2, 1)]
+        [InlineData(19, 11, 17, 9)]
+        public void AddOperatorTest(long prime, long number1, long number2, long result)
+        {
+            var element = new FieldElement(number1, prime) + new FieldElement(number2, prime);
+            Assert.Equal(result, element.Number);
+            Assert.Equal(prime, element.Prime);
+        }
+
+        [Fact]
+        public void SubtractInvalidTest()
+        {
+            Assert.Throws<InvalidOperationException>(() => new FieldElement(1, 3).Subtract(new FieldElement(1, 5)));
+        }
+
+        [Theory]
+        [InlineData(3, 0, 0, 0)]
+        [InlineData(3, 1, 0, 1)]
+        [InlineData(3, 2, 2, 0)]
+        [InlineData(19, 11, 17, 13)]
+        public void SubtractTest(long prime, long number1, long number2, long result)
+        {
+            var element = new FieldElement(number1, prime).Subtract(new FieldElement(number2, prime));
+            Assert.Equal(result, element.Number);
+            Assert.Equal(prime, element.Prime);
+        }
+
+        [Theory]
+        [InlineData(57, 9, 29, 37)]
+        [InlineData(3, 1, 0, 1)]
+        [InlineData(3, 2, 2, 0)]
+        [InlineData(19, 11, 17, 13)]
+        public void SubtractOperatorTest(long prime, long number1, long number2, long result)
+        {
+            var element = new FieldElement(number1, prime) - new FieldElement(number2, prime);
+            Assert.Equal(result, element.Number);
+            Assert.Equal(prime, element.Prime);
+        }
+    }
+}
---
+++ BitcoinBook/FieldElement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BitcoinBook
+{
+    public struct FieldElement
+    {
+        public long Number { get; }
+        public long Prime { get; }
+
+        public FieldElement(long number, long prime)
+        {
+            if (number < 0 || number >= prime)
+            {
+                throw new ArgumentOutOfRangeException($"Number {number} must be in range 0 to {prime-1}");
+            }
+            Number = number;
+            Prime = prime;
+        }
+
+        public FieldElement Add(FieldElement element)
+        {
+            if (element.Prime != Prime) throw new InvalidOperationException("Cannot add two number in different fields");
+
+            return new FieldElement((Number + element.Number) % Prime, Prime);
+        }
+
+        public FieldElement Subtract(FieldElement element)
+        {
+            if (element.Prime != Prime) throw new InvalidOperationException("Cannot add two number in different fields");
+
+            var result = Number - element.Number;
+            if (result < 0)
+            {
+                result += Prime;
+            }
+            return new FieldElement(result % Prime, Prime);
+        }
+
+        public static FieldElement operator +(FieldElement a, FieldElement b) => a.Add(b);
+        public static FieldElement operator -(FieldElement a, FieldElement b) => a.Subtract(b);
+    }
+}
