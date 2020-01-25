a13384c4c2026e02288533d8c0b3ca4c2dbe6835
Tim <tim@timsisland.com>
2020-01-25 09:57:21 -0500

    DerFormat

---
+++ BitcoinBook.Test/SignatureTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace BitcoinBook.Test
+{
+    public class SignatureTest
+    {
+        [Fact]
+        public void DerFormatTest()
+        {
+            Assert.Equal("3045022037206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c60221008ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec",
+                new Signature(
+                    "37206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c6", 
+                    "08ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec").ToDerFormat());
+        }
+    }
+}
--- BitcoinBook/Signature.cs
+++ BitcoinBook/Signature.cs
@@ -23,5 +23,27 @@ namespace BitcoinBook
         {
             return $"Signature({R:X},{S:x})";
         }
+
+        public string ToDerFormat()
+        {
+            var rs = ToPrefixedHex(R) + ToPrefixedHex(S);
+            return $"30{rs.Length/2:X2}{rs}";
+        }
+
+        string ToPrefixedHex(BigInteger i)
+        {
+            var hex = ToHex(i);
+            return $"02{hex.Length/2:X2}{hex}";
+        }
+
+        string ToHex(BigInteger i)
+        {
+            var hex = i.ToString("X64");
+            if (hex.Length == 65 && hex.StartsWith("0"))
+            {
+                hex = '0' + hex;
+            }
+            return hex.ToLower();
+        }
     }
 }
