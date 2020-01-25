69aa02f817f0f116c6fb2b7492bdcf0a6225c3ae
Tim <tim@timsisland.com>
2020-01-25 10:04:56 -0500

    Fixed sig verification tests

--- BitcoinBook.Test/PublicKeyTests.cs
+++ BitcoinBook.Test/PublicKeyTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Numerics;
 using Xunit;
 
@@ -7,28 +8,28 @@ namespace BitcoinBook.Test
     public class PublicKeyTests
     {
         readonly PublicKey key = new PublicKey(
-            "00887387e452b8eacc4acfde10d9aaf7f6d9a0f975aabb10d006e4da568744d06c",
-            "0061de6d95231cd89026e286df3b6ae4a894a3378e393e93a0f45b666329a0ae34");
+            "0887387e452b8eacc4acfde10d9aaf7f6d9a0f975aabb10d006e4da568744d06c",
+            "061de6d95231cd89026e286df3b6ae4a894a3378e393e93a0f45b666329a0ae34");
 
-        [Fact(Skip = "Sigs don't come out the same as book")]
+        [Fact]
         public void SignatureOneTest()
         {
             var signature = new Signature(
-                "00ac8d1c87e51d0d441be8b3dd5b05c8795b48875dffe00b7ffcfac23010d3a395",
-                "0068342ceff8935ededd102dd876ffd6ba72d6a427a3edb13d26eb0781cb423c4");
+                "0ac8d1c87e51d0d441be8b3dd5b05c8795b48875dffe00b7ffcfac23010d3a395",
+                "068342ceff8935ededd102dd876ffd6ba72d6a427a3edb13d26eb0781cb423c4");
             Assert.True(key.Verify(
-                "00ec208baa0fc1c19f708a9ca96fdeff3ac3f230bb4a7ba4aede4942ad003c0f60",
+                BigInteger.Parse("0ec208baa0fc1c19f708a9ca96fdeff3ac3f230bb4a7ba4aede4942ad003c0f60", NumberStyles.HexNumber),
                 signature));
         }
 
-        [Fact(Skip = "Sigs don't come out the same as book")]
+        [Fact]
         public void SignatureTwoTest()
         {
             var signature = new Signature(
                 "00eff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c",
                 "00c7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab6");
             Assert.True(key.Verify(
-                "007c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d",
+                BigInteger.Parse("007c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d", NumberStyles.HexNumber),
                 signature));
         }
 
