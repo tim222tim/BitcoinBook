6800ba4a625e95987b6675b0f6e0eda2228a4cde
Tim <tim@timsisland.com>
2020-01-31 09:06:12 -0500

    Pulled out WriterBase

--- BitcoinBook/TransactionWriter.cs
+++ BitcoinBook/TransactionWriter.cs
@@ -1,26 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Numerics;
 
 namespace BitcoinBook
 {
-    public class TransactionWriter
+    public class TransactionWriter : WriterBase
     {
-        readonly BinaryWriter writer;
-
-        public TransactionWriter(BinaryWriter writer)
+        public TransactionWriter(BinaryWriter writer) : base(writer)
         {
-            this.writer = writer ?? throw new ArgumentNullException(nameof(writer));
         }
 
-        public TransactionWriter(Stream stream) : this(new BinaryWriter(stream ?? throw new ArgumentNullException(nameof(stream))))
+        public TransactionWriter(Stream stream) : base(stream)
         {
         }
 
         public void Write(Transaction transaction)
         {
-            Write((ulong) transaction.Version, 4);
+            Write(transaction.Version, 4);
             Write(transaction.Inputs);
             Write(transaction.Outputs);
             Write(transaction.LockTime, 4);
@@ -43,27 +38,9 @@ namespace BitcoinBook
             Write(input.Sequence, 4);
         }
 
-        void WriteReverse(byte[] bytes)
-        {
-            var newBytes = (byte[])bytes.Clone();
-            Array.Reverse(newBytes);
-            writer.Write(newBytes);
-        }
-
-        void Write(ScriptSig scriptSig)
-        {
-            WriteVarBytes(scriptSig.Bytes);
-        }
-
-        void WriteVarBytes(byte[] bytes)
-        {
-            WriteVar((ulong) bytes.Length);
-            writer.Write(bytes);
-        }
-
         void Write(ICollection<TransactionOutput> outputs)
         {
-            WriteVar((ulong)outputs.Count);
+            WriteVar(outputs.Count);
             foreach (var output in outputs)
             {
                 Write(output);
@@ -72,53 +49,18 @@ namespace BitcoinBook
 
         void Write(TransactionOutput output)
         {
-            Write((ulong)output.Amount, 8);
+            Write(output.Amount, 8);
             Write(output.ScriptPubKey);
+        }
+
+        void Write(ScriptSig scriptSig)
+        {
+            WriteVarBytes(scriptSig.Bytes);
         }
 
         void Write(ScriptPubKey scriptPubKey)
         {
             WriteVarBytes(scriptPubKey.Bytes);
-        }
-
-        public void Write(int i, int length)
-        {
-            Write((ulong)i, length);
-        }
-
-        public void Write(ulong i, int length)
-        {
-            var bytes = new BigInteger(i).ToByteArray();
-            var bx = 0;
-            var wx = 0;
-            while (bx < bytes.Length && wx++ < length)
-            {
-                writer.Write(bytes[bx++]);
-            }
-            while (wx++ < length)
-            {
-                writer.Write((byte)0);
-            }
-        }
-
-        public void WriteVar(ulong i)
-        {
-            var length = GetVarLength(i);
-            if (length > 1)
-            {
-                writer.Write(GetVarPrefix(i));
-            }
-            Write(i, length);
-        }
-
-        int GetVarLength(ulong i)
-        {
-            return i < 0xfd ? 1 : i < 0x10000 ? 2 : i < 0x100000000 ? 4 : 8;
-        }
-
-        byte GetVarPrefix(ulong i)
-        {
-            return i < 0x10000 ? (byte)0xfd : i < 0x100000000 ? (byte)0xfe : (byte)0xff;
         }
     }
 }
---
+++ BitcoinBook/WriterBase.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+
+namespace BitcoinBook
+{
+    public class WriterBase
+    {
+        protected BinaryWriter Writer { get; }
+
+        public WriterBase(BinaryWriter writer)
+        {
+            Writer = writer ?? throw new ArgumentNullException(nameof(writer));
+        }
+
+        public WriterBase(Stream stream) : this(new BinaryWriter(stream ?? throw new ArgumentNullException(nameof(stream))))
+        {
+        }
+
+        public void Write(int i, int length)
+        {
+            Write((ulong)i, length);
+        }
+
+        public void Write(long i, int length)
+        {
+            Write((ulong)i, length);
+        }
+
+        public void Write(ulong i, int length)
+        {
+            var bytes = new BigInteger(i).ToByteArray();
+            var bx = 0;
+            var wx = 0;
+            while (bx < bytes.Length && wx++ < length)
+            {
+                Writer.Write(bytes[bx++]);
+            }
+            while (wx++ < length)
+            {
+                Writer.Write((byte)0);
+            }
+        }
+
+        public void WriteVar(int i)
+        {
+            WriteVar((ulong)i);
+        }
+
+        public void WriteVar(ulong i)
+        {
+            var length = GetVarLength(i);
+            if (length > 1)
+            {
+                Writer.Write(GetVarPrefix(i));
+            }
+            Write(i, length);
+        }
+
+        protected void WriteVarBytes(byte[] bytes)
+        {
+            WriteVar((ulong) bytes.Length);
+            Writer.Write(bytes);
+        }
+
+        protected void WriteReverse(byte[] bytes)
+        {
+            var newBytes = (byte[])bytes.Clone();
+            Array.Reverse(newBytes);
+            Writer.Write(newBytes);
+        }
+
+        int GetVarLength(ulong i)
+        {
+            return i < 0xfd ? 1 : i < 0x10000 ? 2 : i < 0x100000000 ? 4 : 8;
+        }
+
+        byte GetVarPrefix(ulong i)
+        {
+            return i < 0x10000 ? (byte)0xfd : i < 0x100000000 ? (byte)0xfe : (byte)0xff;
+        }
+    }
+}
