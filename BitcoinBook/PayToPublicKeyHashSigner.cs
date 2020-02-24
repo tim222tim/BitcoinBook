80465f129435595794e8ab8513ae2a8cae0fe8c6
Tim <tim@timsisland.com>
2020-02-24 18:02:04 -0500

    pulled out TransactionSignerBase

--- BitcoinBook/PayToPublicKeyHashSigner.cs
+++ BitcoinBook/PayToPublicKeyHashSigner.cs
@@ -2,20 +2,15 @@
 
 namespace BitcoinBook
 {
-    public class PayToPublicKeyHashSigner : ITransactionSigner
+    public class PayToPublicKeyHashSigner : TransactionSignerBase
     {
-        readonly TransactionHasher hasher;
-
-        public PayToPublicKeyHashSigner(TransactionHasher hasher)
+        public PayToPublicKeyHashSigner(TransactionHasher hasher) : base(hasher)
         {
-            this.hasher = hasher;
         }
 
-        public async Task<Script> CreateSigScript(PrivateKey privateKey, Transaction transaction,
-            TransactionInput input, SigHashType sigHashType)
+        public override async Task<Script> CreateSigScript(PrivateKey privateKey, Transaction transaction, TransactionInput input, SigHashType sigHashType)
         {
-            var hash = await hasher.ComputeSigHash(transaction, input, sigHashType);
-            var signature = privateKey.Sign(hash).ToDer().Concat((byte) sigHashType);
+            var signature = await ComputerSignatureBytes(privateKey, transaction, input, sigHashType);
             var sec = privateKey.PublicKey.ToSec();
             return new Script(signature, sec);
         }
---
+++ BitcoinBook/TransactionSignerBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+namespace BitcoinBook
+{
+    public abstract class TransactionSignerBase : ITransactionSigner
+    { 
+        readonly TransactionHasher hasher;
+
+        protected TransactionSignerBase(TransactionHasher hasher)
+        {
+            this.hasher = hasher;
+        }
+
+        public abstract Task<Script> CreateSigScript(PrivateKey privateKey, Transaction transaction, TransactionInput input, SigHashType sigHashType);
+
+        protected async Task<byte[]> ComputerSignatureBytes(PrivateKey privateKey, Transaction transaction, TransactionInput input, SigHashType sigHashType)
+        {
+            var hash = await hasher.ComputeSigHash(transaction, input, sigHashType);
+            return privateKey.Sign(hash).ToDer().Concat((byte) sigHashType);
+        }
+    }
+}
