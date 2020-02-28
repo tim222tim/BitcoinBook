32173e7266a7ca6e9ee57bccc8cae6152abacabd
Tim Rossiter <tim@timsisland.com>
2020-02-28 08:52:46 -0500

    SignerMap

---
+++ BitcoinBook.Test/SignerMapTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using Xunit;
+
+namespace BitcoinBook.Test
+{
+    public class SignerMapTests
+    {
+        readonly SignerMap signerMap;
+
+        public SignerMapTests()
+        {
+            var fetcher = new TransactionFetcher(new HttpClient());
+            var hasher = new TransactionHasher(null);
+
+            signerMap = new SignerMap(
+                new PayToPubKeySigner(fetcher, hasher), 
+                new PayToPubKeyHashSigner(fetcher, hasher));
+        }
+
+        [Theory]
+        [InlineData(typeof(PayToPubKeySigner), ScriptType.PayToPubKey)]
+        [InlineData(typeof(PayToPubKeyHashSigner), ScriptType.PayToPubKeyHash)]
+        public void SignerTest(Type signerType, ScriptType scriptType)
+        {
+            Assert.IsType(signerType, signerMap[scriptType]);
+        }
+    }
+}
---
+++ BitcoinBook/SignerMap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace BitcoinBook
+{
+    public class SignerMap
+    {
+        readonly IDictionary<ScriptType, ITransactionSigner> dictionary;
+
+        public SignerMap(PayToPubKeySigner payToPubKeySigner, PayToPubKeyHashSigner payToPubKeyHashSigner)
+        {
+            dictionary = new Dictionary<ScriptType, ITransactionSigner>
+            {
+                {ScriptType.PayToPubKey, payToPubKeySigner },
+                {ScriptType.PayToPubKeyHash, payToPubKeyHashSigner },
+            };
+        }
+
+        public ITransactionSigner this[ScriptType scriptType] => dictionary[scriptType];
+    }
+}
