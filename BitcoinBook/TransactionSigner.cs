25d68fb9423012cfa94b339cf12600e33d70cce2
Tim Rossiter <tim@timsisland.com>
2020-02-28 09:58:18 -0500

    TransactionSigner -- needs test

--- BitcoinBook/TransactionSigner.cs
+++ BitcoinBook/TransactionSigner.cs
@@ -1,15 +1,51 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace BitcoinBook
 {
     public class TransactionSigner
     {
-        ScriptClassifier scriptClassifier;
-        SignerMap signerMap;
+        readonly ITransactionFetcher fetcher;
+        readonly ScriptClassifier scriptClassifier;
+        readonly SignerMap signerMap;
 
-        public Transaction SignTransaction(Wallet wallet, Transaction transaction)
+        public TransactionSigner(ITransactionFetcher fetcher, ScriptClassifier scriptClassifier, SignerMap signerMap)
         {
-            throw new NotImplementedException();
+            this.fetcher = fetcher;
+            this.scriptClassifier = scriptClassifier;
+            this.signerMap = signerMap;
+        }
+
+        public async Task<Transaction> SignTransaction(Wallet wallet, Transaction transaction, SigHashType sigHashType, bool ignoreKeyNotFound = false)
+        {
+            foreach (var input in transaction.Inputs)
+            {
+                var priorOutput = await fetcher.GetPriorOutput(input);
+
+                var scriptType = scriptClassifier.GetScriptType(priorOutput.ScriptPubKey);
+                var signer = signerMap[scriptType];
+                if (signer != null)
+                {
+                    try
+                    {
+                        var sigScript = await signer.CreateSigScript(wallet, transaction, input, sigHashType);
+                        transaction = transaction.CloneWithReplacedSigScript(input, sigScript);
+                    }
+                    catch (PrivateKeyNotFoundException)
+                    {
+                        if (!ignoreKeyNotFound)
+                        {
+                            throw;
+                        }
+                    }
+                }
+                else if (!ignoreKeyNotFound)
+                {
+                    throw new InvalidOperationException($"Unknown script type for prior output {input.PreviousTransaction}:{input.PreviousIndex}");
+                }
+            }
+
+            return transaction;
         }
     }
 }
