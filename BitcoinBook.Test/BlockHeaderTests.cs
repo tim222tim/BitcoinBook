bed9ad1fade82adec748f8588cbf98d9034e05f6
Tim Rossiter <tim@timsisland.com>
2020-03-20 16:27:18 -0400

    ComputeId doesn't work yet

---
+++ BitcoinBook.Test/BlockHeaderTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace BitcoinBook.Test
+{
+    public class BlockHeaderTests
+    {
+        [Fact]
+        public void ComputeIdTest()
+        {
+            var header = new BlockHeader(2, "0000000000000000007962066dcd6675830883516bcf40047d42740a85eb2919", 
+                "31951c69428a95a46b517ffb0de12fec1bd0b2392aec07b64573e03ded31621f", 1513622125, 0x18009645, 0x5cfc9955);
+            Assert.Equal("00000000000000000024fb37364cbf81fd49cc2d51c09c75c35433c3a1945d04", header.Id);
+        }
+    }
+}
--- BitcoinBook/BlockHeader.cs
+++ BitcoinBook/BlockHeader.cs
@@ -1,24 +1,66 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace BitcoinBook
 {
     public class BlockHeader
     {
+        readonly byte[] id;
+        readonly byte[] previousBlock;
+        readonly byte[] merkleRoot;
+
+        public string Id => id.ToHex();
         public uint Version { get; }
-        public byte[] PreviousBlock { get; }
-        public byte[] MerkleRoot { get; }
+        public string PreviousBlock => previousBlock.ToHex();
+        public string MerkleRoot => merkleRoot.ToHex();
         public uint Timestamp { get; }
         public uint Bits { get; }
         public uint Nonce { get; }
 
-        public BlockHeader(uint version, byte[] previousBlock, byte[] merkleRoot, uint timestamp, uint bits, uint nonce)
+
+        public BlockHeader(uint version, string previousBlock, string merkleRoot, uint timestamp, uint bits, uint nonce)
         {
             Version = version;
-            PreviousBlock = previousBlock;
-            MerkleRoot = merkleRoot;
+            this.previousBlock = Cipher.ToBytes(previousBlock);
+            Array.Reverse(this.previousBlock);
+            this.merkleRoot = Cipher.ToBytes(merkleRoot);
+            Array.Reverse(this.merkleRoot);
             Timestamp = timestamp;
             Bits = bits;
             Nonce = nonce;
+            id = ComputeId();
+        }
+
+        byte[] ComputeId()
+        {
+            var hash = Cipher.Hash256(Cipher.Hash256(ToBytes()));
+            // Array.Reverse(hash);
+            return hash;
+        }
+
+        public byte[] ToBytes()
+        {
+            var bytes = new byte[80];
+            CopyBytes(Version, bytes, 0);
+            Array.Copy(previousBlock, 0, bytes, 4, 32);
+            Array.Copy(merkleRoot, 0, bytes, 36, 32);
+            CopyBytes(Timestamp, bytes, 68);
+            CopyBytes(Bits, bytes, 72);
+            CopyBytes(Nonce, bytes, 76);
+            return bytes;
+        }
+
+        void CopyBytes(uint value, byte[] bytes, int index)
+        {
+            bytes[index] = (byte)value;
+            bytes[++index] = (byte)(value >> 8);
+            bytes[++index] = (byte)(value >> 0x10);
+            bytes[++index] = (byte)(value >> 0x18);
+
+            // bytes[index] = (byte)(value >> 0x18);
+            // bytes[++index] = (byte)(value >> 0x10);
+            // bytes[++index] = (byte)(value >> 8);
+            // bytes[++index] = (byte)value;
         }
     }
 }
