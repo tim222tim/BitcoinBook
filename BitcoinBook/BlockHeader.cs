6b71554e0e98be8dbaaee83ae7b6a44e9423768f
Tim Rossiter <tim@timsisland.com>
2020-03-20 08:27:40 -0400

    BlockHeader

---
+++ BitcoinBook/BlockHeader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BitcoinBook
+{
+    public class BlockHeader
+    {
+        public uint Version { get; }
+        public byte[] PreviousBlock { get; }
+        public byte[] MerkleRoot { get; }
+        public uint Timestamp { get; }
+        public uint Bits { get; }
+        public uint Nonce { get; }
+
+        public BlockHeader(uint version, byte[] previousBlock, byte[] merkleRoot, uint timestamp, uint bits, uint nonce)
+        {
+            Version = version;
+            PreviousBlock = previousBlock;
+            MerkleRoot = merkleRoot;
+            Timestamp = timestamp;
+            Bits = bits;
+            Nonce = nonce;
+        }
+    }
+}
