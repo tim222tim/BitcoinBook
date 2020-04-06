1e183a7c2ce1d4e1430b051a7eb7d57de5c0079f
Tim Rossiter <tim@timsisland.com>
2020-04-06 15:59:30 -0400

    NetworkEnvelope

---
+++ BitcoinBook.Test/NetworkEnvelopeTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace BitcoinBook.Test
+{
+    public class NetworkEnvelopeTests
+    {
+        [Fact]
+        public void ToStringTest()
+        {
+            Assert.Equal("VERSION: 010210", new NetworkEnvelope("VERSION", new byte[] {1, 2, 16}, false).ToString());
+        }
+    }
+}
---
+++ BitcoinBook/NetworkEnvelope.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BitcoinBook
+{
+    public class NetworkEnvelope
+    {
+        public string Command { get; }
+        public byte[] Payload { get; }
+        public bool Testnet { get; }
+
+        public NetworkEnvelope(string command, byte[] payload, bool testnet)
+        {
+            Command = command ?? throw new ArgumentNullException(nameof(command));
+            Payload = payload ?? throw new ArgumentNullException(nameof(payload));
+            Testnet = testnet;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{Command}: {Payload.ToHex()}");
+        }
+    }
+}
