29efac0d0751fe49d8780c0dcaa44a9ba61e2c35
Tim Rossiter <tim@timsisland.com>
2020-04-29 07:52:54 -0400

    SimpleNode

--- BitcoinBook.Test/SimpleNodeTest.cs
+++ BitcoinBook.Test/SimpleNodeTest.cs
@@ -1,16 +1,15 @@
 ﻿using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
-using System.Numerics;
 using Xunit;
 
 namespace BitcoinBook.Test
 {
-    public class SocketTest
+    public class SimpleNodeTest
     {
         public static IEnumerable<object[]> HandshakeData => new[]
         {
-            new object[] { Dns.GetHostEntry("testnet.programmingbitcoin.com").AddressList[0], true },
+            // new object[] { Dns.GetHostEntry("testnet.programmingbitcoin.com").AddressList[0], true },
             new object[] { new IPAddress(new byte[] { 104, 62, 47, 181 }), false},
         };
 
@@ -18,7 +17,10 @@ namespace BitcoinBook.Test
         [MemberData(nameof(HandshakeData))]
         public void HandshakeTest(IPAddress ipAddress, bool testnet)
         {
-            SendVersion(ipAddress, testnet);
+            using var node = new SimpleNode(ipAddress, testnet);
+            node.Handshake();
+            Assert.StartsWith("/Satoshi", node.RemoteUserAgent);
+            // SendVersion(ipAddress, testnet);
         }
 
         void SendVersion(IPAddress ipAddress, bool testnet)
@@ -27,8 +29,7 @@ namespace BitcoinBook.Test
             string agent = null;
 
             var socket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            var port = (ushort) (testnet ? 18333 : 8333);
-            socket.Connect(new IPEndPoint(ipAddress, port));
+            socket.Connect(new IPEndPoint(ipAddress, (ushort) (testnet ? 18333 : 8333)));
 
             try
             {
---
+++ BitcoinBook/SimpleNode.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace BitcoinBook
+{
+    public class SimpleNode : IDisposable
+    {
+        readonly bool testnet;
+
+        readonly Socket socket;
+        readonly NetworkStream stream;
+
+        public string RemoteUserAgent { get; private set; }
+
+        public SimpleNode(IPAddress remoteHost, bool testnet = false)
+        {
+            this.testnet = testnet;
+            socket = new Socket(remoteHost.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(new IPEndPoint(remoteHost, (ushort)(testnet ? 18333 : 8333)));
+            stream = new NetworkStream(socket);
+        }
+
+        public void Handshake()
+        {
+            var gotVerAck = false;
+            string agent = null;
+
+            new NetworkEnvelope(new VersionMessage(), testnet).WriteTo(stream);
+
+            while (agent == null && !gotVerAck)
+            {
+                var responseEnvelope = NetworkEnvelope.Parse(stream, testnet);
+                agent = (responseEnvelope.Message as VersionMessage)?.UserAgent;
+                gotVerAck = responseEnvelope.Message is VerAckMessage;
+            }
+
+            RemoteUserAgent = agent;
+        }
+
+        public void Dispose()
+        {
+            stream?.Dispose();
+            socket?.Dispose();
+        }
+    }
+}
