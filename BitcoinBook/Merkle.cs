a4a70057ebe3c4e4ff131a8a55bdf150812998a7
Tim Rossiter <tim@timsisland.com>
2020-06-03 10:39:18 -0400

    ComputeLevelParent

--- BitcoinBook/Merkle.cs
+++ BitcoinBook/Merkle.cs
@@ -1,10 +1,29 @@
-﻿namespace BitcoinBook
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BitcoinBook
 {
     public class Merkle
     {
         public static byte[] ComputeParent(byte[] hash1, byte[] hash2)
         {
             return Cipher.Hash256(hash1.Concat(hash2));
+        }
+
+        public static IList<byte[]> ComputeLevelParent(IEnumerable<byte[]> hashes)
+        {
+            var hashList = hashes.ToList();
+            if (hashList.Count % 2 == 1)
+            {
+                hashList.Add(hashList[^1]);
+            }
+            var parentHashes = new List<byte[]>();
+            for (var i = 0; i < hashList.Count; i += 2)
+            {
+                parentHashes.Add(ComputeParent(hashList[i], hashList[i+1]));
+            }
+
+            return parentHashes;
         }
     }
 }
