75ef25dd703b19322f6a310bb6ba1d0eb5ec5a27
Tim Rossiter <tim@timsisland.com>
2020-06-03 10:16:40 -0400

    Merkle.ComputeParent

---
+++ BitcoinBook.Test/MerkleTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace BitcoinBook.Test
+{
+    public class MerkleTests
+    {
+        [Fact]
+        public void ComputerParentTest()
+        {
+            var hash1 = Cipher.ToBytes("c117ea8ec828342f4dfb0ad6bd140e03a50720ece40169ee38bdc15d9eb64cf5");
+            var hash2 = Cipher.ToBytes("c131474164b412e3406696da1ee20ab0fc9bf41c8f05fa8ceea7a08d672d7cc5");
+            Assert.Equal("8b30c5ba100f6f2e5ad1e2a742e5020491240f8eb514fe97c713c31718ad7ecd", Merkle.ComputeParent(hash1, hash2).ToHex());
+        }
+    }
+}
---
+++ BitcoinBook/Merkle.cs
@@ -0,0 +1,10 @@
+﻿namespace BitcoinBook
+{
+    public class Merkle
+    {
+        public static byte[] ComputeParent(byte[] hash1, byte[] hash2)
+        {
+            return Cipher.Hash256(hash1.Concat(hash2));
+        }
+    }
+}
