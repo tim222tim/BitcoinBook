33e7a66f2f32e6ecdffdf2016c8afa717c8046e8
Tim Rossiter <tim@timsisland.com>
2020-06-11 07:24:19 -0400

    GetDataMessage

---
+++ BitcoinBook.Test/GetDataMessageTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace BitcoinBook.Test
+{
+    public class GetDataMessageTests
+    {
+        static readonly string[] items = 
+        {
+            "00000000000000000011421889a2d5d0f52f0e73decd0846167fc5e9011dd1cc",
+            "0000000000000000007962066dcd6675830883516bcf40047d42740a85eb2919",
+        };
+        readonly GetDataMessage message = new GetDataMessage(BlockDataType.MerkleBlock, items.Select(Cipher.ToBytes));
+        const string messageHex = "020300000000000000000000000011421889a2d5d0f52f0e73decd0846167fc5e9011dd1cc0000000000000000007962066dcd6675830883516bcf40047d42740a85eb2919";
+
+        [Fact]
+        public void ToBytesTest()
+        {
+            var actual = message.ToBytes().ToHex();
+            Assert.Equal(messageHex, actual);
+        }
+
+        [Fact]
+        public void ParseTest()
+        {
+            var newMessage = GetDataMessage.Parse(Cipher.ToBytes(messageHex));
+            Assert.Equal(message.BlockDataType, newMessage.BlockDataType);
+            Assert.Equal(message.Items.Count, newMessage.Items.Count);
+            for (var i = 0; i < message.Items.Count; i++)
+            {
+                Assert.True(newMessage.Items[i].SequenceEqual(message.Items[i]));
+            }
+        }
+    }
+}
---
+++ BitcoinBook/GetDataMessage.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+
+namespace BitcoinBook
+{
+    public class GetDataMessage : IMessage
+    {
+        readonly List<byte[]> items;
+
+        public string Command => "getdata";
+
+        public BlockDataType BlockDataType { get; }
+
+        public IList<byte[]> Items => items.AsReadOnly();
+
+        public GetDataMessage(BlockDataType blockDataType, IEnumerable<byte[]> items)
+        {
+            if (items == null) throw new ArgumentNullException(nameof(items));
+
+            BlockDataType = blockDataType;
+            this.items = items.ToList();
+
+            CheckHashes(this.items, false);
+        }
+
+        public static GetDataMessage Parse(byte[] bytes)
+        {
+            var reader = new ByteReader(bytes);
+            try
+            {
+                var count = reader.ReadVarInt();
+                var dataType = (BlockDataType)reader.ReadInt(4);
+                var items = new List<byte[]>();
+                while (count-- > 0)
+                {
+                    items.Add(reader.ReadBytes(32));
+                }
+                return new GetDataMessage(dataType, items);
+            }
+            catch (EndOfStreamException ex)
+            {
+                throw new FormatException("Read past end of data", ex);
+            }
+        }
+
+        public byte[] ToBytes()
+        {
+            var stream = new MemoryStream();
+            var writer = new ByteWriter(stream);
+            writer.WriteVar(items.Count);
+            writer.Write((int)BlockDataType, 4);
+            foreach (var item in items)
+            {
+                writer.Write(item);
+            }
+            return stream.ToArray();
+        }
+
+        [SuppressMessage("ReSharper", "ParameterOnlyUsedForPreconditionCheck.Local")]
+        void CheckHashes(List<byte[]> hashes, bool isEmptyAllowed)
+        {
+            if (!(isEmptyAllowed || hashes.Any())) throw new ArgumentException("Must contain at least one hash", nameof(hashes));
+            if (hashes.Any(h => h == null || h.Length != 32)) throw new ArgumentException("All hashes must be 32 bytes", nameof(hashes));
+        }
+    }
+}
