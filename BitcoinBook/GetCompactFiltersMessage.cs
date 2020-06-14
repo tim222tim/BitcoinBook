b480eda8fa5b457c1170e51528940a8d864275e3
Tim Rossiter <tim@timsisland.com>
2020-06-14 10:59:52 -0400

    GetCompactFiltersMessage

---
+++ BitcoinBook/GetCompactFiltersMessage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace BitcoinBook
+{
+    public class GetCompactFiltersMessage : IMessage
+    {
+        public FilterType FilterType { get; }
+        public uint StartHeight { get; }
+        public byte[] StopHash { get; }
+
+        public string Command => "getcfilters";
+
+        public GetCompactFiltersMessage(FilterType filterType, uint startHeight, byte[] stopHash)
+        {
+            FilterType = filterType;
+            StartHeight = startHeight;
+            StopHash = stopHash ?? throw new ArgumentNullException(nameof(stopHash));
+        }
+
+        public static GetCompactFiltersMessage Parse(byte[] bytes)
+        {
+            var reader = new ByteReader(bytes);
+            try
+            {
+                return new GetCompactFiltersMessage(
+                    (FilterType) reader.ReadByte(),
+                    reader.ReadUnsignedInt(4),
+                    reader.ReadBytes(32));
+            }
+            catch (EndOfStreamException ex)
+            {
+                throw new FormatException("Read past end of data", ex);
+            }
+        }
+
+        public byte[] ToBytes()
+        {
+            var stream = new MemoryStream();
+            var writer = new ByteWriter(stream);
+            writer.Write((byte)FilterType);
+            writer.Write(StartHeight, 4);
+            writer.Write(StopHash);
+            return stream.ToArray();
+        }
+    }
+}
