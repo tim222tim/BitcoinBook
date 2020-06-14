666ea11347ef8752273ff816f852f04ed40ba9cb
Tim Rossiter <tim@timsisland.com>
2020-06-14 07:18:05 -0400

    bloom filters are disabled

--- BitcoinBook.Test/SimpleNodeTest.cs
+++ BitcoinBook.Test/SimpleNodeTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace BitcoinBook.Test
@@ -18,7 +19,7 @@ namespace BitcoinBook.Test
         {
             timNode?.Dispose();
         }
-
+        
         [Theory]
         [MemberData(nameof(NodeData))]
         public void HandshakeTest(NodeSetting setting)
@@ -48,17 +49,23 @@ namespace BitcoinBook.Test
             Assert.Equal(previousId, message.BlockHeaders[0].PreviousBlock);
         }
 
-        // [Fact]
+        // bloom filters are removed
         // public void GetDataTest()
         // {
         //     timNode.Handshake();
         //     var filter = new BloomFilter(30, 5, 90210);
         //     filter.Add(Address.HashFromAddress("1CK6KHY6MHgYvmRQ4PAafKYDrg1ejbH1cE"));
-        //     timNode.Send(new FilterLoadMessage());
+        //     timNode.Send(new FilterLoadMessage(filter));
         //     var previousId = "0000000000000000001475235ddf33e99c1b560d20179392e3592546ca654552";
         //     timNode.Send(new GetHeadersMessage(previousId));
-        //     var message = timNode.WaitFor<HeadersMessage>();
-        //     Assert.NotNull(message);
+        //     var headersMessage = timNode.WaitFor<HeadersMessage>();
+        //     Assert.NotNull(headersMessage);
+        //     var blockDataItems = headersMessage.BlockHeaders.Select(h => new BlockDataItem(BlockDataType.MerkleBlock, Cipher.ToBytes(h.Id)));
+        //     timNode.Send(new GetDataMessage(blockDataItems));
+        //     while (true)
+        //     {
+        //         var message = timNode.WaitForMessage();
+        //     }
         // }
     }
 }
