1d2d2e7e29bff6157ba0b3e8d75f1908c4b2799f
Tim Rossiter <tim@timsisland.com>
2020-06-16 07:52:15 -0400

    Remove TimeStamp

--- BitcoinBook/NetworkAddress.cs
+++ BitcoinBook/NetworkAddress.cs
@@ -7,18 +7,16 @@ namespace BitcoinBook
         public ulong Services { get; }
         public IPAddress IPAddress { get; }
         public ushort Port { get; }
-        public long TimeStamp { get; }
 
         public NetworkAddress() : this(0, new IPAddress(new byte[] { 0, 0, 0, 0 }), 0)
         {
         }
 
-        public NetworkAddress(ulong services, IPAddress address, ushort port, long timeStamp = 0)
+        public NetworkAddress(ulong services, IPAddress address, ushort port)
         {
             Services = services;
             IPAddress = address;
             Port = port;
-            TimeStamp = timeStamp;
         }
     }
 }
